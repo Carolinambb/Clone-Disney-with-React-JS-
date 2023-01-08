cff85c2862238e418377bedce328fce3f6f8ccde
carolinambb <carolinariveiro50@gmail.com>
2023-01-08 15:44:09 -0300

    adding logo and bg

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,16 @@
 import styled from 'styled-components';
 
+
 const Login = (props) =>{
     return (
         <Container>
-        <Content>Content</Content>
+        <Content>
+            <CTA>
+            <CTALogoOne src="/images/cta-logo-one.svg"/>
+
+            </CTA>
+            <BgImage />
+            </Content>
        </Container>
        )
    
@@ -30,4 +37,46 @@ flex-direction: column;
 height: 100%;
 `
 
+const BgImage = styled.div`
+  height: 100%;
+  background-position: top;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-image: url("/images/login-background.jpg");
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  z-index: -1;
+`;
+
+const CTA = styled.div `
+/* margin-bottom: 2vw;
+max-width: 650px;
+flex-wrap: wrap;
+display: flex;
+flex-direction: column;
+justify-content: center;
+margin-top: 0;
+align-items: center;
+margin-right: auto;
+margin-left: auto;
+transition-timing-function: ease-out;
+transition: opacity 0.2;
+width: 100%; */
+
+max-width: 650px;
+width: 100%;
+display: flex;
+flex-direction: column;
+
+`
+
+const CTALogoOne = styled.img`
+margin-bottom: 12px;
+max-width: 600px;
+min-height: 1px;
+display: block;
+width: 100%;
+`
 export default Login;
